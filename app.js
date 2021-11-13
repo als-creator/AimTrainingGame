aca29446e21de9a6370f7bf54c2346f746ec9512
Aleksandr <mrstuzer@gmail.com>
2021-11-13 23:08:12 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="styles.css" />
-    <title>Aim Training | Проект 5</title>
-  </head>
-  <body>
-    <div class="screen">
-      <h1>Aim Training</h1>
-      <a href="#" class="start" id="start">Начать игру</a>
-    </div>
+const startBtn = document.querySelector("#start")
+const screens = document.querySelectorAll(".screen")
+const timeList = document.querySelector("#time-list")
+const timeEl = document.querySelector("#time")
+const board = document.querySelector ("#board")
+const colors = ['#DC143C', '#FF0000', '#F08080', '#AFEEEE', '#FF1493', '#DB7093']
+let time = 0
+let score = 0
 
-    <div class="screen">
-      <h1>Выберите время</h1>
-      <ul class="time-list" id="time-list">
-        <li>
-          <button class="time-btn"
-          data-time="10">
-            10 сек
-          </button>
-        </li>
-        <li>
-          <button class="time-btn"
-          data-time="20">
-            20 сек
-          </button>
-        </li>
-        <li>
-          <button class="time-btn"
-          data-time="30">
-            30 сек
-          </button>
-        </li>
-        <li>
-          <button class="time-btn"
-          data-time="40">
-            40 сек
-          </button>
-        </li>
-      </ul>
-    </div>
+startBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    screens[0].classList.add("up")
+})
 
-    <div class="screen">
-      <h3>Осталось <span id="time">00:00</span></h3>
-      <div class="board" id="board"></div>
-    </div>
-    <script src="app.js"></script>
-  </body>
-</html>
+timeList.addEventListener("click", event =>
+{
+    if(event.target.classList.contains
+        ("time-btn")) {
+        time = parseInt(event.target.getAttribute("data-time"))
+        screens[1].classList.add("up")
+        startGame()
+    }
+
+})
+
+board.addEventListener("click", event => {
+if (event.target.classList.contains("circle")) {
+    score++
+    event.target.remove()
+    createRandomCircle()
+}
+})
+
+function startGame(){
+   setInterval(decreaseTime, 1000)
+   createRandomCircle()
+    setTime(time)
+}
+
+function decreaseTime(){
+    if (time===0){
+finishGame()
+ } else {let current = --time
+    if(current < 10) {
+        current = `0${current}`}
+
+setTime (current)
+}
+}
+
+function setTime(value) {
+    timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+board.innerHTML = `<h1> Счет: ${score} </h1>`
+}
+
+function createRandomCircle() {
+    const circle = document.createElement ("div")
+    const size = getRandomNumber (10,60)
+    const {width, height} = board.getBoundingClientRect()
+    const x=getRandomNumber(0, width-size)
+    const y=getRandomNumber(0, height-size)
+    circle.classList.add ("circle")
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+    circle.style.backgroundColor = getRandomColor()
+    board.append(circle)
+
+}
+function getRandomNumber(min,max) {
+    return Math.round(Math.random()*(max-min)+min)
+}
+
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
